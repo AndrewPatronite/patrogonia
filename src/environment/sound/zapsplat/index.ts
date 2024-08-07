449cdf30784c727b27afe100190804629448b05c
Andrew Patronite <patronite@gmail.com>
2024-08-07 16:59:39 -0700

    Upgraded redux toolkit to version addressing webpack issue. Updated mp3 imports that stopped working with other updates.

--- src/environment/sound/zapsplat/index.ts
+++ src/environment/sound/zapsplat/index.ts
@@ -1,13 +1,9 @@
-const HealingSound = require('./zapsplat_fantasy_magic_mystery_glissando_bell_43990.mp3')
-  .default
-const IceSound = require('./sound_spark_Ice_Spell_Elsas_Frozen_Death_Touch_01.mp3')
-  .default
-const LevelUpSound = require('./cartoon_success_fanfair.mp3').default
-const PartyDestroyedSound = require('./cartoon_fail_strings_trumpet.mp3')
-  .default
-const WarpSound = require('./magic_spell_ascending_metallic_pad.mp3').default
-const TalkingSound = require('./zapsplat_multimedia_game_sound_retro_blip_022_29554.mp3')
-  .default
+import HealingSound from './zapsplat_fantasy_magic_mystery_glissando_bell_43990.mp3';
+import IceSound from './sound_spark_Ice_Spell_Elsas_Frozen_Death_Touch_01.mp3';
+import LevelUpSound from './cartoon_success_fanfair.mp3';
+import PartyDestroyedSound from './cartoon_fail_strings_trumpet.mp3';
+import WarpSound from './magic_spell_ascending_metallic_pad.mp3';
+import TalkingSound from './zapsplat_multimedia_game_sound_retro_blip_022_29554.mp3';
 
 export {
   HealingSound,
